533d117ac0a171261f955b6908342791adce78b5
fiona23 <fiona_fanmy@163.com>
2015-03-25 21:08:28 +0800

    base style

--- _site/public/js/blog.js
+++ _site/public/js/blog.js
@@ -1,9 +1,9 @@
 Layout('.index-img', 0);
 Layout('.blog-block',1);
+headImg();
 
 function Layout(event, num){
     $event = $(event);
-	console.log($event)
     for (var i=0; i<$event.length; i++){
 	var con;
 	if(num == 0){con = (i%2==0)}else{con = (i%2!=0)} 
@@ -19,4 +19,16 @@ function Layout(event, num){
         	})
         }
     }    
+}
+
+function headImg(){
+	var $img = $('#index-head').find('img');
+	var $p = $('#describe');
+	var imgWidth = $img.width();
+	var pWidth = $p.width();
+	var  bodyWidth = $('body').width();
+	var imgLeft = parseInt((bodyWidth - imgWidth-pWidth)/2+pWidth);
+	$img.css("left",imgLeft+'px');
+	$p.css("left",parseInt((bodyWidth - imgWidth-pWidth)/2)+'px')
+
 }
