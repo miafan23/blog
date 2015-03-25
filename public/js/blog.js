bc3656c6edeea5b5a2bc7afe06e51e7330fd30b4
fiona23 <fiona_fanmy@163.com>
2015-03-25 19:33:05 +0800

    layout

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,9 +1,13 @@
 Layout('.index-img', 0);
+Layout('.blog-block',1);
 
 function Layout(event, num){
-    $event = $(event)
-    for (var i=num; i<$event.length; i++){
-        if(i%2 ==0 ){
+    $event = $(event);
+	console.log($event)
+    for (var i=0; i<$event.length; i++){
+	var con;
+	if(num == 0){con = (i%2==0)}else{con = (i%2!=0)} 
+        if(con ){
         	$event.eq(i).css({
         		"left":'0',
         		"top":i*400+'px'
@@ -11,11 +15,8 @@ function Layout(event, num){
         }else{
         	$event.eq(i).css({
         		"right":'0',
-        		"top":"400px"
+        		"top":i*400+'px'
         	})
         }
     }    
 }
-
-
-
