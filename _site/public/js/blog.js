1a912f98483de8ad368125d6ecfaa4858dfc9192
fiona23 <fiona_fanmy@163.com>
2015-04-03 09:53:00 +0800

    blog list complete details

--- _site/public/js/blog.js
+++ _site/public/js/blog.js
@@ -40,18 +40,21 @@ function headImg(){
 function bloglist(){
 	var $bloglist = $('#blog-list').find('li');
 	var opacity = 0;
+	var padding = 30;
 	var i=1;
 	var timer;
 	fade(i);
 	function fade(num){
-		if(opacity >= 1) {
+		if(opacity >= 1 && num<$bloglist.length) {
 		clearTimeout(timer);
+		padding=30;
 		opacity = 0;
 		fade(num+1);
 		}else{	
-	opacity = opacity+0.1;
-	$bloglist.eq(num).css('opacity',opacity);
-	timer = setTimeout(rfade(num),50)}
+	opacity = opacity+0.05;
+	padding += 1
+	$bloglist.eq(num).css({'opacity':opacity,'padding-top':padding+'px'});
+	timer = setTimeout(rfade(num),20)}
 	}
 	function rfade(num){
 		return function(){
