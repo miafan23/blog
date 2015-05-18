c2dc024cfca05426a66462a750a16cea5c55ee65
fiona23 <fiona_fanmy@163.com>
2015-05-18 21:31:16 +0800

    new style

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,41 +1,4 @@
-Layout('.index-img', 0);
-Layout('.blog-block',1);
-headImg();
 bloglist();
-function Layout(event, num){
-    $event = $(event);
-    for (var i=0; i<$event.length; i++){
-	var con;
-	if(num == 0){con = (i%2==0)}else{con = (i%2!=0)} 
-        if(con ){
-        	$event.eq(i).css({
-        		"left":'0',
-        		"top":i*400+'px'
-        	})
-        }else{
-        	$event.eq(i).css({
-        		"right":'0',
-        		"top":i*400+'px'
-        	})
-        }
-    }    
-}
-
-function headImg(){
-	var $img = $('#index-head').find('img');
-	var $p = $('#describe');
-	var imgWidth = $img.width();
-	var pWidth = $p.width();
-	var  bodyWidth = $('body').width();
-	var imgLeft = parseInt((bodyWidth - imgWidth-pWidth)/2+pWidth);
-	var $content = $('#index-content');
-	var contentLeft = parseInt((bodyWidth-$content.width())/2)
-	var $ul = $('#index-head').find('ul');
-	$img.css("left",imgLeft+'px');
-	$p.css("left",parseInt((bodyWidth - imgWidth-pWidth)/2)+'px')
-	$ul.css("left",contentLeft+'px')
-
-}
 
 function bloglist(){
 	var $bloglist = $('#blog-list').find('li');
