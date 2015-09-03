a8392418e6cc4914d163f73002993be76dbe2b20
fiona23 <fiona_fanmy@163.com>
2015-09-03 23:16:23 +0800

    update responsive

--- _site/public/js/blog.js
+++ _site/public/js/blog.js
@@ -11,3 +11,39 @@ $('#hamburger').click(function(){
         nav.css('height', '0')
     }
 })
+
+/*
+$.mouseWheel = function (selector, listener) {
+    function handleMouseWheel (event) {
+        var e = event || window.event;
+        target = e.srcElement? e.srcElement : e.target;
+        e.preventDefault();
+        e.stopPropagation();
+        var delta;
+        if (e.wheelDelta) {
+            delta = e.wheelDelta;
+        }
+        else {
+            delta = -e.detail * 40;
+        }
+        //像下滚为负值
+        listener(delta, target)
+    }
+    $.on(selector, "mousewheel", function (event) {
+        handleMouseWheel(event)
+    });
+
+    $.on(selector, "DOMMouseScroll", function (event) {
+        handleMouseWheel(event)
+    })
+}
+
+
+$.mouseWheel('body', function (delta) {
+    if (delta < 0) {
+        $('header').eq(0).addClass('down')
+    } else {
+        $('header').eq(0).removeClass('down')
+    }
+})
+*/
